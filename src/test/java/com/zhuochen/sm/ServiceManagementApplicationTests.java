0bcae812ced6e0ef7b31a53825e47dd94c39fd13
zhuochen_shen <szc186!?szc>
2018-08-20 14:46:21 +0800

    init

---
+++ src/test/java/com/zhuochen/sm/ServiceManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhuochen.sm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
